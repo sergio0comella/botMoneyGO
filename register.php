6424da4fc0e298db8039c6c8aba638f8859a7f36
Sergio Comella <32553024+Panezio923@users.noreply.github.com>
2019-09-05 12:27:58 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,1 +1,17 @@
-<?php// PARAMETRI DA MODIFICARE$WEBHOOK_URL = 'https://infinite-stream-xxxxx.herokuapp.com/execute.php';$BOT_TOKEN = ' ';// NON APPORTARE MODIFICHE NEL CODICE SEGUENTE$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';$method = 'setWebhook';$parameters = array('url' => $WEBHOOK_URL);$url = $API_URL . $method. '?' . http_build_query($parameters);$handle = curl_init($url);curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);curl_setopt($handle, CURLOPT_TIMEOUT, 60);$result = curl_exec($handle);print_r($result);
+<?php
+
+// PARAMETRI DA MODIFICARE
+$WEBHOOK_URL = 'https://intense-thicket-28731.herokuapp.com/execute.php';
+$BOT_TOKEN = '978219762:AAHaFe80I5p2Rlooe7dEN3KaGLJIxiyxReE';
+
+// NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
+$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
+$method = 'setWebhook';
+$parameters = array('url' => $WEBHOOK_URL);
+$url = $API_URL . $method. '?' . http_build_query($parameters);
+$handle = curl_init($url);
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($handle, CURLOPT_TIMEOUT, 60);
+$result = curl_exec($handle);
+print_r($result);
